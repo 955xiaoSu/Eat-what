53c4d1ac62aa2851547b02b606d0bbea386ed5e3
God.Su <93633273+955xiaoSu@users.noreply.github.com>
2023-06-04 16:29:17 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,8 +6,8 @@ print("Called by crontab")
 def send_email(to_email, subject, body):
     smtp_server = 'smtp.qq.com'  # QQ 邮箱的 SMTP 服务器地址
     smtp_port = 465  # QQ 邮箱的 SMTP 服务器端口号
-    sender_email = 'kaisuping@qq.com'  # 发送方邮箱
-    sender_password = 'abhqipuaujjycbac' # 发送方邮件授权码
+    sender_email = '填入发送方邮箱'  
+    sender_password = '填入发送方邮箱授权码' 
 
     message = MIMEText(body, 'plain', 'utf-8')
     message['Subject'] = subject
@@ -30,7 +30,7 @@ with open('email_list.txt', 'r') as file:
 # 循环发送邮件给每个邮箱
 for email in email_list:
     to_email = email.strip()  # 去除首尾空白字符
-    subject = '订阅邮箱测试'
+    subject = '订阅邮箱测试'   # 根据自己的需求修改 subject、body
     body = '这是一封测试邮件。'
     send_email(to_email, subject, body)
 
